6c81c323b49442c460dc536bf12defd5d4231f7e
asis-parajuli <60426026+asis-parajuli@users.noreply.github.com>
2020-05-11 23:00:21 +0545

    Django + Angular

---
+++ Scrum Master Django + Angular/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!e:\self study\django+angular\scrum_master\djangular\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ Scrum Master Django + Angular/djangular/scrumboard/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+
+from .models import List, Card
+
+class CardSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = '__all__'
+        model = Card
+
+
+class ListSerializer(serializers.ModelSerializer):
+    cards = CardSerializer(read_only=True, many=True)
+    class Meta:
+        fields = '__all__'
+        model = List
